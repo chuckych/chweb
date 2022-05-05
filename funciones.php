080779f5d2f0f3a5545b0cf64c805ae612eb1150
Norberto CH <nchaquer@gmail.com>
2022-05-05 10:39:52 -0300

    v0.0.225

--- funciones.php
+++ funciones.php
@@ -2,7 +2,7 @@
 // use PhpOffice\PhpSpreadsheet\Worksheet\Row;
 function version()
 {
-    return 'v0.0.224'; // Version de la aplicación
+    return 'v0.0.225'; // Version de la aplicación
 }
 function verDBLocal()
 {
