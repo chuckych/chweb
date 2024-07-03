8574f2de8964ffef9b88344d98d0db4439479720
Norberto CH <nchaquer@gmail.com>
2024-07-02 21:29:09 -0300

    v0.4.85

--- js/jquery.php
+++ js/jquery.php
@@ -1,5 +1,4 @@
 <!-- jquery.min.js -->
-<script src="https://polyfill.io/v3/polyfill.js?features=es5,es6,es7&flags=gated"></script>
 <script src="/<?= HOMEHOST ?>/js/jquery-3.5.1-min.js"></script>
 <script src="/<?= HOMEHOST ?>/js/popper.min.js"></script>
 <!-- bootstrap.min.js -->
--- mobile/usuarios/index.php
+++
@@ -1,7 +0,0 @@
-<?php
-session_start();
-require __DIR__ . '../../../config/index.php';
-secure_auth_ch();
-$Modulo='26';
-ExisteModRol($Modulo);
-require pagina('usuarios.php');
