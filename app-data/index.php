f2d31762c054c6c27815f6d1e1e9c61bd38bee7b
Norberto CH <nchaquer@gmail.com>
2025-09-12 10:30:16 -0300

    v0.8.18

--- app-data/index.php
+++ app-data/index.php
@@ -1334,8 +1334,8 @@ Flight::route('POST /prysmian/@tipo', function ($tipo) {
                             foreach ($filas as $fila) {
                                 if (!is_array($fila))
                                     continue;
-                                // Excluir los registros de Domingo
-                                if (($fila['Dia'] ?? '') === 'Domingo')
+                                // Excluir los registros de Domingo y Sábado
+                                if (($fila['Dia'] ?? '') === 'Domingo' || ($fila['Dia'] ?? '') === 'Sábado')
                                     continue;
 
                                 if (!array_key_exists('HsDelDia', $fila))
@@ -1350,13 +1350,148 @@ Flight::route('POST /prysmian/@tipo', function ($tipo) {
                         return $out;
                     };
 
+                    $sumarMinutosPorClave2 = static function (array $data): array {
+                        $out = [];
+                        foreach ($data as $clave => $filas) {
+                            $total = 0;
+                            if (!is_array($filas)) {
+                                $out[$clave] = 0;
+                                continue;
+                            }
+                            foreach ($filas as $fila) {
+                                if (!is_array($fila))
+                                    continue;
+                                // Excluir los registros de Domingo y Sábado
+                                if (($fila['Dia'] ?? '') !== 'Sábado')
+                                    continue;
+
+                                if (!array_key_exists('HsDelDia', $fila))
+                                    continue;
+                                $v = $fila['HsDelDia'];
+                                if ($v === null || $v === '')
+                                    continue; // sólo si no es null ni vacío
+                                $total += to_minutes($v);
+                            }
+                            $out[$clave] = $total;
+                        }
+                        return $out;
+                    };
+
+                    $sumarMinutosPorClaveSabado = static function (array $data): array {
+                        $out = [];
+                        foreach ($data as $clave => $filas) {
+                            if (!is_array($filas)) {
+                                $out[$clave] = [];
+                                continue;
+                            }
+                            foreach ($filas as $fila) {
+                                if (!is_array($fila))
+                                    continue;
+                                // Excluir los registros que no sean de Sábado
+                                if (($fila['Dia'] ?? '') !== 'Sábado')
+                                    continue;
+
+                                // Agregar toda la fila completa al array de resultados
+                                $out[$clave][] = $fila;
+                            }
+                        }
+                        return $out;
+                    };
+
                     $datosHorasDia = cacheData(
                         "horas-dia-{$hashPayload}",
                         $payloadHorasDia,
                         'horas dia',
                         '/horarios/asignados'
                     );
+
+                    // Flight::json([
+                    //     // 'dataficNoveHora' => count($dataficNoveHora) ?? [],
+                    //     // 'idealesHorasDiaSabado' => ($idealesHorasDiaSabado) ?? [],
+                    //     'datosHorasDia' => (
+                    //         $datosHorasDia
+                    //         // 'Legajo',
+                    //         // 'HsDelDia'
+                    //     ) ?? [],
+                    // ]);
+                    // exit;
+
                     $idealesHorasDia = $sumarMinutosPorClave($datosHorasDia);
+                    $idealesHorasDiaSabado = $sumarMinutosPorClaveSabado($datosHorasDia);
+
+                    // buscar las horas de los sabados solo si tiene el tipo de hora 23 y 90
+                    $ficNoveHora = fic_nove_horas([
+                        'FechIni' => $payload['FechIni'] ?? null,
+                        'FechFin' => $payload['FechFin'] ?? null,
+                        'getHor' => 1,
+                        'Hora' => [23, 90],
+                        'HoraEx' => 1,
+                        // 'Lega' => [23],
+                        'Dias' => [7],
+                        'length' => 10000
+                    ]) ?? [];
+
+
+                    $dataficNoveHora = [];
+                    // Agrupar los registros por legajo
+                    foreach ($ficNoveHora as $registro) {
+                        $lega = $registro['Lega'];
+                        if (!isset($dataficNoveHora[$lega])) {
+                            $dataficNoveHora[$lega] = [];
+                        }
+                        $dataficNoveHora[$lega][] = $registro;
+                    }
+                    ksort($dataficNoveHora);
+
+                    // dataficNoveHora es un array asociativo donde la clave el valor del legajo
+                    // idealesHorasDiaSabado es un array asociativo donde la clave el valor del legajo
+
+                    // filtrar idealesHorasDiaSabado y obtener solo los registros que existan en el array dataficNoveHora según su key principal.
+
+                    // $idealesHorasDiaSabadoFiltrado = array_intersect_key($idealesHorasDiaSabado, $dataficNoveHora);
+                    $idealesHorasDiaSabadoFiltrado = array_filter($idealesHorasDiaSabado, function ($key) use ($dataficNoveHora) {
+                        return array_key_exists($key, $dataficNoveHora);
+                    }, ARRAY_FILTER_USE_KEY);
+
+
+                    $fechasDisponibles = [];
+                    foreach ($dataficNoveHora as $legajo => $registros) {
+                        foreach ($registros as $registro) {
+                            $fechasDisponibles[$legajo][] = $registro['Fech'];
+                        }
+                    }
+
+                    // Filtrar idealesHorasDiaSabadoFiltrado comparando fechas
+                    $idealesHorasDiaSabadoFinal = [];
+                    foreach ($idealesHorasDiaSabadoFiltrado as $legajo => $horarios) {
+                        if (isset($fechasDisponibles[$legajo])) {
+                            foreach ($horarios as $horario) {
+                                if (in_array($horario['Fecha'], $fechasDisponibles[$legajo])) {
+                                    $idealesHorasDiaSabadoFinal[$legajo][] = $horario;
+                                }
+                            }
+                        }
+                    }
+
+
+                    // Flight::json([
+                    //     // 'idealesHorasDia' => ($idealesHorasDia) ?? [],
+                    //     // 'dataficNoveHora' => $dataficNoveHora ?? [],
+                    //     // // 'idealesHorasDiaSabado' => $idealesHorasDiaSabado ?? [],
+                    //     // 'idealesHorasDiaSabadoFiltrado' => (
+                    //     //     $idealesHorasDiaSabadoFiltrado
+                    //     // ) ?? [],
+                    //     'idealesHorasDiaSabadoFinal' => (
+                    //         $idealesHorasDiaSabadoFinal
+                    //     ) ?? [],
+                    //     'idealesHorasDiaSabadoFinalSuma' => $sumarMinutosPorClave2($idealesHorasDiaSabadoFinal)
+                    // ]); 
+                    // exit;
+
+                    // Flight::json($idealesHorasDiaSabadoFinal);
+                    // exit;
+
+                    $idealesSabado = $sumarMinutosPorClave2($idealesHorasDiaSabadoFinal);
 
                     // $resultado = array_filter($resultado, fn($v) => $v > 5280);
 
@@ -1419,8 +1554,8 @@ Flight::route('POST /prysmian/@tipo', function ($tipo) {
                     $valoresCustom = array_column($valoresCustom, null, 'descripcion');
 
                     foreach ($clavesCustom as $key => $clave) {
-                        $codigosNove = explode(',', $valoresCustom[$key]['valores'] ?? '') ?? []; // Obtener los codigos de novedad de 'valoresCustom' segun $key y convertirlos en un array
-                        $payload['Nove'] = $codigosNove;
+                        $codNove = explode(',', $valoresCustom[$key]['valores'] ?? '') ?? []; // Obtener los códigos de novedad de 'valoresCustom' según $key y convertirlos en un array
+                        $payload['Nove'] = $codNove;
                         $legajosColumn = horas_custom($legajosColumn, $payload, $key);
                         unset($payload['Nove']);
                     }
@@ -1441,14 +1576,29 @@ Flight::route('POST /prysmian/@tipo', function ($tipo) {
 
                     $tipoHora = array_column($tipoHora, null, 'THoCodi');
                     $strMerienda = $tipoHora['50']['THoDesc2'] ?? 'MERIENDA'; // Obtener la descripcion 2 de 'HORAS MERIENDA'
+                    $strMerienda2 = $tipoHora['51']['THoDesc2'] ?? 'MER2'; // Obtener la descripcion 2 de 'MER2'
                     $strNormales = $tipoHora['90']['THoDesc2'] ?? 'NORMAL'; // Obtener la descripcion 2 de 'HORAS NORMALES'
 
                     foreach ($legajosColumn as $key => $value) {
                         $horasATrabajar = $value['HsATr'] ?? 0;
                         $horasIdeales = $value['Ideales'] ?? 0;
+                        // sumar las idealesSabado a Ideales comparando por la $key
+                        // $horasIdeales = $horasIdeales + ($idealesSabado[$key] ?? 0);
                         $horasMerienda = $value[$strMerienda] ?? 0;
+                        $horasMerienda2 = $value[$strMerienda2] ?? 0;
                         $horasNormales = $value[$strNormales] ?? 0;
-                        $varias = array_sum([$horasATrabajar, $horasMerienda]);
+
+                        $idealesSab = $idealesSabado[$key] ?? 0;
+
+                        $meriendaTotal = $horasMerienda + $horasMerienda2;
+
+                        $legajosColumn[$key]['IdealesSinSabado'] = $horasIdeales - $meriendaTotal;
+                        $legajosColumn[$key]['IdealesSabado'] = $idealesSab;
+                        $Ideales = $horasIdeales - $meriendaTotal + $idealesSab;
+                        $legajosColumn[$key]['Ideales'] = $Ideales;
+
+
+                        $varias = array_sum([$Ideales, $meriendaTotal]);
 
                         $variasARestar = array_sum([
                             $horasNormales,
@@ -1463,8 +1613,10 @@ Flight::route('POST /prysmian/@tipo', function ($tipo) {
 
                         $calculoVarias = $varias - $variasARestar;
                         $legajosColumn[$key]['Varias'] = $calculoVarias >= 0 ? $calculoVarias : 0;
-                        $legajosColumn[$key]['Ideales'] = $horasIdeales - $horasMerienda;
                     }
+
+                    // Flight::json([$idealesSabado, $legajosColumn]);
+                    // exit;
 
                     // recorrer legajosColumn y todos los valores que sean del tipo integer aplicarle al funcion minutos_a_horas_decimal
                     array_walk_recursive($legajosColumn, function (&$item, $key) {
@@ -1476,7 +1628,6 @@ Flight::route('POST /prysmian/@tipo', function ($tipo) {
                     $columnasSorted = $valoresCustom['columnasSorted']['valores'] ?? '';
                     $columnasSorted = explode(',', $columnasSorted);
                     $columnasSorted = array_map('intval', $columnasSorted);
-
 
                     $initKeys = [
                         'FicSectStr' => [
