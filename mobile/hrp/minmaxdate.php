c5b875ec2b9e52397cb41f99be35bd4268516ff3
Norberto CH <nchaquer@gmail.com>
2025-11-05 07:53:32 -0300

    v0.10.11

--- mobile/hrp/minmaxdate.php
+++ mobile/hrp/minmaxdate.php
@@ -6,18 +6,13 @@ secure_auth_ch_json();
 E_ALL();
 header("Content-Type: application/json");
 $data = [];
-/** Para dateRangePicker */
-// $arrayFech = (fecha_min_max_mysql('reg_', 'fechaHora'));
-// $query = "SELECT MIN(fechaHora) AS 'min', MAX(fechaHora) AS 'max' FROM reg_ WHERE id_company = '$_SESSION[ID_CLIENTE]'";
 
-$idCompany = $_SESSION['ID_CLIENTE'];
-$api = "api/v1/checks/dates.php?key=$_SESSION[RECID_CLIENTE]";
-$url = $_SESSION["APIMOBILEHRP"] . "/" . HOMEHOST . "/mobile/hrp/" . $api;
+$idCompany = $_SESSION['ID_CLIENTE_MOBILE'] ?? $_SESSION['ID_CLIENTE_MOBILE'];
+$api = "api/v1/checks/dates.php?key=$_SESSION[RECID_CLIENTE_MOBILE]";
+$url = $_SESSION["APIMOBILEHRP_MOBILE"] . "/" . HOMEHOST . "/mobile/hrp/" . $api;
 $api = getRemoteFile($url, $timeout = 10);
 $api = json_decode($api, true);
 $arrayFech = $api['RESPONSE_DATA'];
-
-// $arrayFech = simple_pdoQuery($query);
 
 $min = !empty($arrayFech['min']) ? FechaFormatVar($arrayFech['min'], 'd-m-Y') : date('d-m-Y');
 $minFormat = !empty($arrayFech['min']) ? FechaFormatVar($arrayFech['min'], 'd/m/Y') : date('d/m/Y');
